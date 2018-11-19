f473ae92a006a57ef9d95fd37226d7d8be3cf685
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2018-11-19 11:16:29 +0100

    Application: new class with main application
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

---
+++ fpeditor_setup.py
@@ -0,0 +1,28 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gio, GdkPixbuf, Gdk
+import sys
+
+from fpeditor.Application import ApplicationWindow
+
+
+class Application(Gtk.Application):
+    def __init__(self):
+        Gtk.Application.__init__(self,
+                                 application_id='eu.mienkofax',
+                                 flags=Gio.ApplicationFlags.HANDLES_OPEN)
+
+        self.window = None
+
+    def do_activate(self):
+        if not self.window:
+            self.window = ApplicationWindow(self)
+
+
+def main():
+    app = Application()
+    app.run(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
